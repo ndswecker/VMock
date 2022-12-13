3e5e6410c27f89fa5896ff0c543992de1d078265
Nathanael Swecker <nathanael.swecker@gmail.com>
2022-12-13 13:32:38 -0800

    Display content index zero

--- react-app/v-mock/src/components/Preamble.js
+++ react-app/v-mock/src/components/Preamble.js
@@ -1,25 +1,37 @@
 import React, { useState } from "react";
 import Select from "react-select";
 import Insult from "../model/Insult";
-import PreambleJSON from "../words/PreambleJSON";
+import { PreambleJSON } from "../words/PreambleJSON";
 
 export default function Preamble() {
     const key = "preamble";
     const[intro, setIntro] = useState();
     const insult = new Insult();
 
-    const preambles = PreambleJSON;
     const options = [];
-    for (let x of preambles.types) {
+    for (let x of PreambleJSON) {
         let opt = {value: x.type, label: x.type};
         options.push(opt);
+    }
+
+    function getContent() {
+        let type = insult.insult.preamble;
+        let contentList = [];
+        for (let t of PreambleJSON) {
+            if (t.type === type){
+                contentList = t.content;
+            }
+        }
+        return contentList;
     }
 
     function preambleSet(opt) {
         setIntro(opt.value);
         insult.addProp(key, opt.value);
-    
+        setIntro(getContent()[0]);
     }
+
+    
 
     return (
         <>
