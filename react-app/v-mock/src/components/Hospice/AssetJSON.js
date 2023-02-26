64b9a6c83898bd05f76ed6132e221790fd31dd52
Nathanael Swecker <nathanael.swecker@gmail.com>
2023-02-25 18:04:48 -0800

    Last entry was influenza partially complete

---
+++ react-app/v-mock/src/components/Hospice/AssetJSON.js
@@ -0,0 +1,170 @@
+/* eslint-disable no-multi-str */
+export const AssetJSON = [
+    {
+        "name": "Connection and Conversation with Family (equipment)",
+        "id": 1,
+        "type": "Experience",
+        "text": 
+            "Have an everyday conversation with family \
+            about disability, especially in \
+            long-standing places of disconnection, offer opportunities to \
+            experiment with an alternative \
+            lens, encourage critical questions, and learn to listen for \
+            deeper stories.",
+        "benefit": "+X to family",
+        "source": "Schnell, 2018"
+    },
+    {
+        "name": "Affective Balance (potion)",
+        "id": 2,
+        "type": "Perspective",
+        "text": "Having more positive emotions than negative emotions in the \
+            last 30 days",
+        "benefit": "+X to Grandparent",
+        "source" : "Leong 2022"
+
+    },
+    {
+        "name": "Mastery (potion)",
+        "id": 3,
+        "type": "Perspective",
+        "text": "Example perspective: \
+            What happens to me in the future mostly depends on me.",
+        "benefit": "+X to Grandparent",
+        "source" : "Leong 2022"
+    },
+    {
+        "name": "Decreased Perceived Constraints (potion)",
+        "id": 4,
+        "type": "Perspective",
+        "text": "Opposite perspective: \
+            What happens in my life is often beyond my control.",
+        "benefit": "+X to Grandparent",
+        "source" : "Leong 2022"
+    },
+    {
+        "name": "Optimism (potion)",
+        "id": 5,
+        "type": "Perspective",
+        "text": "Example perspective: \
+            In uncertain times, I usually expect the best.",
+        "benefit": "+X to Grandparent",
+        "source" : "Leong 2022"
+    },
+    {
+        "name": "Less than High School Diploma",
+        "id": 6,
+        "type": "Education",
+        "text": "",
+        "benefit": "-X to Grandparent income",
+        "source" : "Economic Development Guide 2021"
+    },
+    {
+        "name": "Only a High School Diploma",
+        "id": 7,
+        "type": "Education",
+        "text": "",
+        "benefit": "-X to Grandparent income",
+        "source" : "Economic Development Guide 2021"
+    },
+    {
+        "name": "Some College",
+        "id": 8,
+        "type": "Education",
+        "text": "",
+        "benefit": "No impact on income (relative to others)",
+        "source" : "Economic Development Guide 2021"
+    },
+    {
+        "name": "Bachelor's of Arts Degree (highest degree)",
+        "id": 9,
+        "type": "Education",
+        "text": "",
+        "benefit": "+X to Grandparent income",
+        "source" : "Economic Development Guide 2021"
+    },
+    {
+        "name": "Bachelors of Science Degree (highest degree)",
+        "id": 10,
+        "type": "Education",
+        "text": "",
+        "benefit": "+X to Grandparent income",
+        "source" : "Economic Development Guide 2021"
+    },
+    {
+        "name": "Master's Degree (highest degree)",
+        "id": 11,
+        "type": "Education",
+        "text": "",
+        "benefit": "+X to Grandparent income",
+        "source" : "Economic Development Guide 2021"
+    },
+    {
+        "name": "Doctoral Degree (highest degree)",
+        "id": 12,
+        "type": "Education",
+        "text": "Non medical",
+        "benefit": "+X to Grandparent income",
+        "source" : "Economic Development Guide 2021"
+    },
+    {
+        "name": "Professional Degree",
+        "id": 13,
+        "type": "Education",
+        "text": "Computer Science, Math, Health Practice, and engineering are \
+            the highest paying occupations",
+        "benefit": "+X to Grandparent income",
+        "source" : "Economic Development Guide 2021"
+    },
+    {
+        "name": "John Deer Mobility Scooter 4x4",
+        "id": 14,
+        "type": "Durable Medical Equipment (DME)",
+        "text": "Medicare part B will only cover powered wheelchairs \
+            if they are deemed medically necessary. You will need prior \
+            authorization, and will likely pay 20% once you meet your deductable",
+        "benefit": "+X to Grandparent. Only available to grandparent level X or above.",
+        "source" : "https://www.medicare.gov/coverage/wheelchairs-scooters"
+    },
+    {
+        "name": "Prada Walker (carbon fiber)",
+        "id": 15,
+        "type": "Durable Medical Equipment (DME)",
+        "text": "Medicare part B will only cover walkers \
+            if they are deemed medically necessary. Your doctor must perscribe \
+            it for use in your home.",
+        "benefit": "+X to Grandparent. Only available to grandparent level X or above.",
+        "source" : "https://www.medicare.gov/coverage/walkers"
+    },
+    {
+        "name": "Portable Oxygen Tank of Very High Pressure",
+        "id": 16,
+        "type": "Durable Medical Equipment (DME)",
+        "text": "Medicare part B will only cover oxygen tanks once all criteria have been met \
+            Doctor perscription with severe lung disease, \
+            Your health might improve from oxygen therapy, \
+            Your test results are within a specific range, \
+            Other methods you tried in the past have failed.",
+        "benefit": "+X to Grandparent. Only available to grandparent level X or above.",
+        "source" : "https://www.medicare.gov/coverage/oxygen-equipment-accessories"
+    },
+    {
+        "name": "Blood Sugar Monitor of Invasive Proportion",
+        "id": 17,
+        "type": "Durable Medical Equipment (DME)",
+        "text": "Medicare part B will cover with doctor perscription \
+            After you meet the Part B deductable you will still need \
+            to pay 20%.",
+        "benefit": "+X to Grandparent. Only available to grandparent with diabetes.",
+        "source" : "https://www.medicare.gov/coverage/oxygen-equipment-accessories"
+    }, 
+    {
+        "name": "Diabetes Type One",
+        "id": 18,
+        "type": "Medical Condition",
+        "text": "A chronic condition with onset in childhood or adolescence \
+            where the pancreas makes little to no insulin. No known cure.",
+        "benefit": "-X to grandparent vs any medical condition.",
+        "source": "Mayo Clinic 2023"
+    }
+]
