357e39f9d454d4cdd890d091116b2a280c5a16fd
Nathanael Swecker <nathanael.swecker@gmail.com>
2023-05-09 19:22:48 -0700

    WordVerb getters and setters

--- react-app/v-mock/src/Utility/WordVerb.js
+++ react-app/v-mock/src/Utility/WordVerb.js
@@ -22,4 +22,51 @@ export default class WordVerb extends WordNode {
         this.isGerund = gerund;
     }
 
+    setIsDynamic(dynamic) {
+        this.isDynamic = dynamic;
+    }
+    
+    setIsStative(stative) {
+        this.isStative = stative;
+    }
+
+    setIsTransitive(transitive){
+        this.isTransitive = transitive;
+    }
+
+    setIsLinking(linking) {
+        this.isLinking = linking;
+    }
+
+    setIsInfinitive(infinitive) {
+        this.isInfinitive = infinitive;
+    }
+
+    setIsGerund(gerund) {
+        this.isGerund = gerund;
+    }
+
+    isVerbDynamic() {
+        return this.isDynamic;
+    }
+
+    isVerbStative() {
+        return this.isStative;
+    }
+
+    isVerbTransitive() {
+        return this.isTransitive;
+    }
+
+    isVerbLinking() {
+        return this.isLinking;
+    }
+
+    isVerbInfinitive() {
+        return this.isInfinitive;
+    }
+
+    isVerbGerund() {
+        return this.isGerund;
+    }
 }
