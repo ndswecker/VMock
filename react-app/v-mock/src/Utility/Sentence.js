09bee67399f38be3ec040281d0c765ead8eb0340
Nathanael Swecker <nathanael.swecker@gmail.com>
2023-05-23 12:45:58 -0700

    combine shared subjects appropriately
    needs more testing

--- react-app/v-mock/src/Utility/Sentence.js
+++ react-app/v-mock/src/Utility/Sentence.js
@@ -45,14 +45,29 @@ export default class Sentence {
             // Get each verbs separately
             for (let v = 0; v < this.verbCollection.size(); v++) {
 
-                // Non starting verbs spacer
+                let prevSubject;
+                let currentSubject = this.verbCollection.getVerb(v).getSubject();
+                
                 if (v != 0) {
+                    // Non starting verbs spacer
                     text += " ";
-                }
+                    prevSubject = this.verbCollection.getVerb(v-1).getSubject();
 
-                let verb = this.verbCollection.getVerb(v);
-                text += verb.getSubject();
-                text += ` ${verb.toString()}`;
+                    if (prevSubject === currentSubject) {
+                        console.log("Matching subject from previous verb");
+                        // skip the additon of the subject
+                        text += `${this.verbCollection.getVerb(v).toString()}`;
+                    } else {
+                        console.log("New subject found");
+                        text += this.verbCollection.getVerb(v).getSubject();
+                        text += ` ${this.verbCollection.getVerb(v).toString()}`;
+                    }
+                    
+                } else {
+                    text += this.verbCollection.getVerb(v).getSubject();
+                    text += ` ${this.verbCollection.getVerb(v).toString()}`;
+                }
+                
 
                 // Middle verbs
                 if (v != this.verbCollection.size() - 1) {
